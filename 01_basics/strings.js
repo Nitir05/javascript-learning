62e81bfdea9dea184ac6be4b0a84bf927eeb4b05
Nitir05 <nrahangdal86@gmail.com>
2023-11-28 13:01:47 +0530

    Basics chapter 01

---
+++ 01_basics/strings.js
@@ -0,0 +1,21 @@
+let name = "Nitn";
+let repoCount = 50;
+
+// console.log(name + repoCount); //old way
+// console.log(`Hello i am  ${name} and my repo count is ${repoCount}`); //new way
+
+const nameobj = new String("Shubham");
+// console.log(nameobj[0]);
+// console.log(nameobj.__proto__);
+// console.log(nameobj.toUpperCase());
+
+const newString1 = "     nitin ";
+console.log(newString1.trim()); //removes whitespaces
+
+const url = "https://nitin.com/nitin%20rahangdale";
+console.log(url.replace("%20", "-"));
+
+const splitInArray = "nitin-r-24";
+console.log(splitInArray.split("-"));
+
+const anchor = nitin;
